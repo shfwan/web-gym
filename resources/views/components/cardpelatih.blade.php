1b02df00f2a381a4675878fcf92f40de3f632c48
shfwan <shafwan367@gmail.com>
2024-11-15 12:45:41 +0900

    fix foto  pelatih

--- resources/views/components/cardpelatih.blade.php
+++ resources/views/components/cardpelatih.blade.php
@@ -3,16 +3,17 @@
     'name' => '',
     'description' => '',
     'price' => 0,
+    // 'tersedia' => '',
     'onclick' => ''
 ])
 
 <div class="card bg-white rounded-md border hover:shadow transition-all min-w-72 max-h-fit cursor-pointer" onclick="{{ $onclick }}">
-    <figure class="px-4 pt-4 max-w-96">
-        <img src="{{ asset('storage/upload/' . $picture) }}" alt="Shoes" class="rounded-md" />
+    <figure class="p-4  max-w-full ">
+        <img class="flex-[1_0_100%] max-h-96 rounded-md aspect-square object-cover size-full bg-gray-300" src="{{ asset('storage/upload/' . $picture) }}" alt="Shoes" class="rounded-md" />
     </figure>
     <div class="card-body items-start text-start">
         <h2 class="card-title text-lg text-black">{{ $name }}</h2>
-        <h2 class="card-title text-base text-gray-400">Senin, Selasa, Rabu</h2>
+        {{-- <h2 class="card-title text-base text-gray-400"></h2> --}}
         <p class="text-wrap overflow-hidden text-sm">{{ $description }}</p>
         <h2 class="text-gray-800">@currency($price)</h2>
         <div class="card-actions justify-end w-full">
